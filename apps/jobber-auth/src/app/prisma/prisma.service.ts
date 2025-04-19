476e37752093b82ed22d8dfaca8ee38e28c4eca5
nayan458 <nayancoding@gmail.com>
2025-04-19 13:39:23 +0530

    graphql dependecies installed

--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/jobber-auth';
+import { PrismaClient } from '@prisma-client/jobber-auth';
 
 
 @Injectable()
