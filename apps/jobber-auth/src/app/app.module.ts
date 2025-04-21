30d584e1f04d42d3a83af54442d84a46ed6c3b59
nayan458 <nayancoding@gmail.com>
2025-04-21 11:00:27 +0530

    validation and graphql mutation and basic query

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -9,8 +10,8 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-      sortSchema: true,
     }),
+    UsersModule,
   ],
 })
 export class AppModule {}
---
+++ apps/jobber-auth/src/app/users/users.resolver.ts
@@ -0,0 +1,23 @@
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { User } from './users.model';
+import { UsersService } from './users.service';
+import { CreateUserInput } from './dto/create-user.input';
+
+@Resolver(()=> User)
+export class UsersResolver {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Mutation(() => User)
+    async createUser(
+        @Args('createUserInput') createUserInput: CreateUserInput
+    ) {
+        return this.usersService.createUser(createUserInput);
+    }
+
+    @Query(() => [User], { name: 'users' })
+    async getUsers() {
+        return this.usersService.getUsers();
+    }
+
+    
+}
