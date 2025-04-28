b561bdeb502b7eee9b82e8d6422698d718304707
nayan458 <nayancoding@gmail.com>
2025-04-28 14:48:21 +0530

    failed to apply validation

--- apps/jobber-jobs/src/app/jobs/abstract.job.ts
+++ apps/jobber-jobs/src/app/jobs/abstract.job.ts
@@ -1,13 +1,19 @@
 import { Producer } from 'pulsar-client';
 import { PulsarClient } from '@jobber/pulsar';
 import { serialize } from '@jobber/pulsar';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { BadRequestException } from '@nestjs/common';
 
-export abstract class AbstractJob<T> {
+export abstract class AbstractJob<T extends object> {
   private producer: Producer;
+  protected abstract messageClass: new () => T;
 
   constructor(private readonly pulsarClient: PulsarClient) {}
 
   async execute(data: T, job: string) {
+    this.vaidateData(data);
+
     if (!this.producer) {
       this.producer = await this.pulsarClient.createProducer(job);
     }
@@ -16,5 +22,12 @@ export abstract class AbstractJob<T> {
       // data: Buffer.from(JSON.stringify(data))
       data: serialize(data),
     });
+  }
+
+  private async vaidateData(data: T) {
+    const errors = await validate(plainToInstance(this.messageClass, data));
+    if (errors.length) {
+      throw new BadRequestException(`Job is invalid ${JSON.stringify(errors)}`);
+    }
   }
 }
